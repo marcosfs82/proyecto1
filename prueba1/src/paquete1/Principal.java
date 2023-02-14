8aa7b0e01bc570d26a0ec0826d4781c5fd1ae0e1
Tarde <Tarde@B12-14>
2023-02-14 15:59:31 +0100

    otro archivo mas

--- prueba1/src/paquete1/Principal.java
+++ prueba1/src/paquete1/Principal.java
@@ -3,5 +3,6 @@ package paquete1;
 public class Principal {
 public static void main(String[] args) {
 	System.out.println("Hola mundo");
+	System.out.println("El cambio a la clase Principal");
 }
 }
